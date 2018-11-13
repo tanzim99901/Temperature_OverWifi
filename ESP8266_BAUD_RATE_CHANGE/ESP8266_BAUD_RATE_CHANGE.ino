f60891a65da04316d0c7130278d2254d4ba9d96d
tanzim99901 <44937261+tanzim99901@users.noreply.github.com>
2018-11-13 13:21:24 +0600

    Add files via upload

---
+++ ESP8266_BAUD_RATE_CHANGE/ESP8266_BAUD_RATE_CHANGE.ino
@@ -0,0 +1,61 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial SoftSerial(62, 63);  //RX,TX
+char ssid[] = "Zeus";            // your network SSID (name)
+char pass[] = "google123456";        // your network password
+#define default_baud_rate 9600  // set this as the default baud rate of the ESP8266 module  
+String target_baud_rate = "9600"; // set this as the baud rate you want to change into
+int target;
+
+void setup()
+{
+  // Setup WiFI
+  Serial.begin(9600);
+  target  = target_baud_rate.toInt();
+  // initialize serial for ESP module
+  SoftSerial.begin(default_baud_rate);
+  while(!Serial.available();
+  SoftSerial.println("AT+RST");
+  Serial.println("AT+RST");
+  char value;
+  while (SoftSerial.available()) {
+    value = SoftSerial.read();
+    Serial.println(value);
+
+  }
+  SoftSerial.println(F("AT"));
+  Serial.println(F("AT"));
+  delay(1000);
+
+  while (SoftSerial.available()) {
+    value = SoftSerial.read();
+    Serial.println(value);
+
+  }
+
+  // Baud rates above about 38,000 do not work reliably on the 328p (Pro Mini)
+  Serial.println("AT+UART_DEF=" + target_baud_rate + ",8,1,0,0");
+  SoftSerial.println("AT+UART_DEF=" + target_baud_rate + ",8,1,0,0");
+  delay(1000);
+
+
+  while (SoftSerial.available()) {
+    value = SoftSerial.read();
+    Serial.println(value);
+
+  }
+  // Restart SoftwareSerial for the slower baud rate for the WiFi
+
+  SoftSerial.end();
+  SoftSerial.begin(target);
+
+}
+
+void loop()
+{
+  
+}
+
+
+
+
---
+++ ESP8266_at_test/ESP8266_at_test.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(62, 63);  //RX,TX
+
+void setup()
+{
+  Serial.begin(9600);     // communication with the host computer
+  while (!Serial.available());
+
+  // Start the software serial for communication with the ESP8266
+  mySerial.begin(9600); // set this according to the default baud rate of ESP8266
+
+  Serial.println("");
+  Serial.println("Remember to to set Both NL & CR in the serial monitor.");
+  Serial.println("Ready");
+  Serial.println("");
+}
+
+void loop()
+{
+  // listen for communication from the ESP8266 and then write it to the serial monitor
+  if ( mySerial.available() )   {
+    Serial.write( mySerial.read() );
+  }
+
+  // listen for user input and send it to the ESP8266
+  if ( Serial.available() )       {
+    mySerial.write( Serial.read() );
+  }
+}
+
+
